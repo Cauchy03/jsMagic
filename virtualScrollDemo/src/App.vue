fe3dfd86ba54815d1dc455db12f940f6e9c601f4
Cauchy03 <1481856337@qq.com>
2023-07-22 12:04:03 +0800

    :rocket: feat: test vite-plugin-c-markdown

--- virtualScrollDemo/src/App.vue
+++ virtualScrollDemo/src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <virtualScroll />
-  <input type="file" name="" id="">
+  <!-- <virtualScroll /> -->
+  <!-- <input type="file" name="" id=""> -->
+  <cmarkdown></cmarkdown>
 </template>
 
 <script setup lang="ts">
-import virtualScroll from './components/virtualScroll.vue'
-console.log(new ArrayBuffer())
+// import virtualScroll from './components/virtualScroll.vue'
+import cmarkdown from './components/testMd.vue' 
 </script>
 
 <style scoped></style>
--- virtualScrollDemo/vite.config.ts
+++ virtualScrollDemo/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import markdown from 'vite-plugin-c-markdown'
+
+// @ts-ignore
+// import markdown from '../../vite-plugin-markdown/dist/index.esm'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // @ts-ignore
+    markdown()
+  ],
   resolve:{
     alias: {
       '@': path.resolve('./src') // 相对路径别名配置，使用 @ 代替 src
