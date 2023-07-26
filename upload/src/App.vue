a377a61e94c781e78b38db7abe141f2058c9ddb8
Cauchy03 <1481856337@qq.com>
2023-07-26 14:46:52 +0800

    :sparkles: feat: add chunk progress

--- upload/src/App.vue
+++ upload/src/App.vue
@@ -2,17 +2,20 @@
   <div>
     <input type="file" @change="handleFileChange">
     <button @click="handleUpload">上传</button>
+    <div style="width: 500px; margin-top: 20px;">
+      <a-progress  size="small" v-for="(item, index) in data " :key="index" :percent="item.percent"/>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { reactive, ref } from 'vue';
+import request from './utils/request'
+
 // 切片大小
 // the chunk size 10MB
 // const SIZE = 10 * 1024 * 1024;  // 1kb = 1024 字节
-
-const SIZE = 100 * 1024
-import { reactive, ref } from 'vue';
-import request from './utils/request'
+const SIZE = 10 * 1024 * 1024
 
 const container = reactive({
   file: null
@@ -45,53 +48,60 @@ function createFileChunk(file: any, size = SIZE) {
 // 上传切片
 async function uploadChunks() {
   const requestList = data.value
-    .map(({ chunk, hash }) => {
+    .map(({ chunk, hash, index }) => {
       const formData = new FormData();
       formData.append("chunk", chunk);
       formData.append("hash", hash);
       formData.append("filename", container.file.name);
       // FormData对象有一个特点，将文件信息添加进去后，直接打印不能看到文件信息，需要使用for of遍历才能看到
       return {
-        formData
+        formData,
+        index
       }
     })
-    .map(({ formData }) =>
+    .map(({ formData, index }) =>
       request({
         url: "http://localhost:3000",
-        data: formData
+        data: formData,
+        onProgress: e => {
+          data.value[index].percent = Number((e.loaded / e.total * 100).toFixed(2))
+        }
       })
     )
   // 并发请求
   await Promise.all(requestList)
   // 合并切片
-  // await mergeRequest();
+  await mergeRequest()
 }
 
 // 合并切片
-// const mergeRequest = async () => {
-//   await request({
-//     url: "http://localhost:3000/merge",
-//     headers: {
-//       "content-type": "application/json"
-//     },
-//     data: JSON.stringify({
-//       filename: container.file.name
-//     })
-//   })
-// }
+const mergeRequest = async () => {
+  await request({
+    url: "http://localhost:3000/merge",
+    headers: {
+      "content-type": "application/json"
+    },
+    data: JSON.stringify({
+      size: SIZE,
+      filename: container.file.name
+    })
+  })
+
+}
 
 // 上传
-
-
 const handleUpload = async () => {
   if (!container.file) return;
   const fileChunkList = createFileChunk(container.file);
   data.value = fileChunkList.map(({ file }, index) => ({
     chunk: file,
     // 文件名 + 数组下标
-    hash: container.file.name + "-" + index
-  }));
+    hash: container.file.name + "-" + index,
+    index, // 索引 
+    percent: 0  // 记录每个切片上传进度
+  }))
   await uploadChunks();
+  console.log(data.value)
 }
 
 </script>
--- upload/src/utils/request.ts
+++ upload/src/utils/request.ts
@@ -3,29 +3,32 @@ interface Options {
   method: string,
   data: any,
   headers: any,
-  requestList: any
+  requestList: any,
+  onProgress: Function
 }
 
-interface x extends Partial<Options>{}
+interface x extends Partial<Options> { }
 
 export default ({
   url,
   method = "post",
   data,
   headers = {},
-}: x) => {
-  return new Promise(resolve => {
-    const xhr = new XMLHttpRequest()
-    xhr.open(method, url)
-    Object.keys(headers).forEach(key =>
-      xhr.setRequestHeader(key, headers[key])
-    )
-    xhr.send(data)
-    xhr.onload = (e: any) => {
-      console.log(e.target.response)
-      resolve({
-        data: e.target.response
-      })
-    }
-  })
-}
+  onProgress = e => e
+}: x) => new Promise(resolve => {
+  const xhr = new XMLHttpRequest()
+  xhr.open(method, url)
+  Object.keys(headers).forEach(key =>
+    xhr.setRequestHeader(key, headers[key])
+  )
+  xhr.send(data)
+  // @ts-ignore
+  xhr.addEventListener('progress', onProgress)
+  xhr.onload = (e: any) => {
+    console.log(e.target.response)
+    resolve({
+      data: e.target.response
+    })
+  }
+})
+
